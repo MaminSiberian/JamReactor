b6717ca6f810190c19e9ab9eb452cf0d6f4681b3
ZhekaRifer <scorpionrulit2@bk.ru>
2023-09-30 18:49:11 +0300

    Add traectory enemy

--- Assets/Game/Enemy's/Scripts/MovementEnemyOnSpots.cs
+++ Assets/Game/Enemy's/Scripts/MovementEnemyOnSpots.cs
@@ -12,22 +12,28 @@ public class MovementEnemyOnSpots : MonoBehaviour
     [Header("Timer")]    
     
     public float startMoveTime;
+    public bool isRotate;
     private float waitTime;
     private bool temp = true;
     private Rigidbody2D _rb;
+    [SerializeField] private Animator _anim;
 
     private void Start()
     {
         waitTime = startMoveTime;
         countSpot = 0;
         _rb = GetComponent<Rigidbody2D>();
+        transform.position = moveSpots[0].position;
     }
     private void Update()
     {
-        
-        Vector2 lookDir = moveSpots[countSpot].position - transform.position;
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-        _rb.rotation = angle;
+
+        if (isRotate)
+        {
+            Vector2 lookDir = moveSpots[countSpot].position - transform.position;
+            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+            _rb.rotation = angle;
+        }
         transform.position = Vector2.MoveTowards(transform.position, moveSpots[countSpot].position, speed * Time.deltaTime);
         
         if (Vector2.Distance(transform.position, moveSpots[countSpot].position) < 0.1f)
@@ -63,15 +69,15 @@ public class MovementEnemyOnSpots : MonoBehaviour
     }
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("Col");
+        
         if(collision.collider.CompareTag("Player"))
         {
-            Debug.Log("Player");
+            _anim.SetTrigger("Attack");
             var direction = collision.transform.position - transform.position;
             collision.gameObject.GetComponent<MoveTest>().fall = true;
             collision.rigidbody.AddForce(direction * forcePush,
                 ForceMode2D.Impulse); ;
-            Debug.Log(collision.rigidbody.velocity);
+            
         }
     }
     
