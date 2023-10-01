f478c6dd8aaeea87e2ade7828a98cf4deaff09cb
MaminSiberian <120947225+MaminSiberian@users.noreply.github.com>
2023-10-01 22:19:05 +0300

    Добавил паузу

---
+++ Assets/Game/Pause/MusicSlider.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicSlider : MonoBehaviour
+{
+    [SerializeField] private Pause pause;
+    private Slider slider;
+
+    private void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+    private void OnEnable()
+    {
+        slider.onValueChanged.AddListener(OnValueChanged);
+    }
+    private void OnDisable()
+    {
+        slider?.onValueChanged.RemoveListener(OnValueChanged);
+    }
+    private void OnValueChanged(float value)
+    {
+        pause.SetMusicVolume(value);
+    }
+}
