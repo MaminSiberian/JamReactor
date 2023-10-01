7fffed14501b51f13b1f7a75bb41d722b9570aad
ZhekaRifer <scorpionrulit2@bk.ru>
2023-10-01 19:22:05 +0300

    Ad deathsounds

--- Assets/BoomScripts.cs
+++ Assets/BoomScripts.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 
 public class BoomScripts : MonoBehaviour
 {
+    private AudioSource m_AudioSource;
+    public AudioClip[] deathSound;
     
     void Start()
     {
         StartCoroutine(Timer());
+        m_AudioSource = GetComponent<AudioSource>();
+        m_AudioSource.PlayOneShot(deathSound[Random.Range(0,deathSound.Length - 1)]);
     }
     IEnumerator Timer()
     {
